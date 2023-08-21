c231411e468a9a6642105d2c35b8536d46f52a65
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-08-21 16:39:09 +0100

    added axios

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Searching ${keyword}`);
   }
 
   function showKeyWord(event) {
     setKeyword(event.target.value);
   }
+  function handleResponse(response) {
+    console.log(response.data.word);
+  }
+  const apiKey = "9c350f182b3bcf281a5dbac65ff4ot92";
+  let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
 
   return (
     <div className="Dictionary">
